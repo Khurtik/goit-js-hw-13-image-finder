bc7f4e9eea0bd56b1d989ef109b1dda37ca46ae3
sergiy khurtik <skhurtik@ukr.net>
2019-11-19 23:37:02 +0200

    hw-13

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,17 +1,41 @@
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 const baseUrl = 'https://pixabay.com/api/';
+const API_KEY = '14297998-d1283a06c8d7c6782bc49ad77';
 
 export default {
   page: 1,
-  fetchImg(query) {
+  query: '',
+  fetchImg() {
     const options = {
       headers: {
-        authorization: '14297998-d1283a06c8d7c6782bc49ad77',
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        // authorization: '14297998-d1283a06c8d7c6782bc49ad77',
       },
     };
-    const requestParams = `?image_type=photo&orientation=horizontal&q=${query}&page=${this.page}&per_page=12&key=14297998-d1283a06c8d7c6782bc49ad77`;
-    return fetch(proxyUrl + baseUrl + requestParams, options).then(response =>
-      response.json(),
-    );
+    const requestParams = `?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${API_KEY}`;
+    return fetch(proxyUrl + baseUrl + requestParams, options)
+      .then(response => {
+        return response.json();
+      })
+      .then(parseResponsed => {
+        this.incrementPage();
+        return parseResponsed.hits;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  get searchQuery() {
+    return this.query;
+  },
+  set searchQuery(string) {
+    this.query = string;
+  },
+  incrementPage() {
+    this.page += 1;
+  },
+  resetPage() {
+    this.page = 1;
   },
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,65 @@
-import apiService from './apiService';
+import apiService from './apiService.js';
+import imgItemTemplate from '../template/img-items.hbs';
+import PNotify from '../../node_modules/pnotify/dist/es/PNotify.js';
+import '../../node_modules/pnotify/dist/es/PNotifyStyleMaterial.js';
+import '../css/style.css';
+import '../../node_modules/material-design-icons';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   galleryList: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('button[data-action="loadmore"]'),
 };
 
 refs.searchForm.addEventListener('submit', searchFormSubmitHeandler);
+refs.loadMoreBtn.addEventListener('click', loadMoreBtnHeandler);
+refs.loadMoreBtn.addEventListener('click', () =>
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  }),
+);
 
 function searchFormSubmitHeandler(e) {
   e.preventDefault();
+  const form = e.currentTarget;
+  const input = form.elements.query;
 
-  const searchQuery = e.currentTarget.elements.query.value;
+  clearListItems();
 
-  console.log(searchQuery);
+  apiService.resetPage();
+  apiService.searchQuery = input.value;
 
-  apiService.fetchImg(searchQuery).then(data => console.log(data));
+  apiService.fetchImg().then(hits => {
+    // const markup = imgItemTemplate(hits);
+    // refs.galleryList.insertAdjacentHTML('beforeend', markup);
+
+    if (!hits) {
+      refs.galleryList.innerHTML = '';
+    } else if (hits <= 0) {
+      PNotify.defaults.styling = 'material';
+      PNotify.error({
+        title: 'Oh No!',
+        text: 'Запрос некорректный.Попробуйте еще раз',
+      });
+    } else {
+      const markup = imgItemTemplate(hits);
+      refs.galleryList.insertAdjacentHTML('beforeend', markup);
+    }
+  });
+
+  input.value = '';
+}
+
+function loadMoreBtnHeandler() {
+  apiService.fetchImg().then(insertListItem);
+}
+
+function insertListItem(items) {
+  const markup = imgItemTemplate(items);
+  refs.galleryList.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearListItems() {
+  refs.galleryList.innerHTML = '';
 }
